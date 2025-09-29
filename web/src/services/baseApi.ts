1c33477425fce5324ddfab67be4478747114a5c5
abhijithpnuam <abhijith.p@nuamenterprises.com>
2025-09-29 16:52:30 +0530

    Update baseApi.ts

--- web/src/services/baseApi.ts
+++ web/src/services/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "@/app/store";
 
 export const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_API_URL || "http://localhost:3000/api/v1",
+  baseUrl: import.meta.env.VITE_API_URL,
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.accessToken;
     if (token) {
